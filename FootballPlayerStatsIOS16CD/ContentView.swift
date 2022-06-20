7b34afd96b883dcf54e3562cef1a78d47c51392e
pottsie <pottsma@me.com>
2022-06-20 15:08:41 -0400

    Initial Commit

---
+++ FootballPlayerStatsIOS16CD/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  FootballPlayerStatsIOS16CD
+//
+//  Created by Michael Potts on 6/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FootballPlayerStatsIOS16CD/FootballPlayerStatsIOS16CDApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballPlayerStatsIOS16CDApp.swift
+//  FootballPlayerStatsIOS16CD
+//
+//  Created by Michael Potts on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct FootballPlayerStatsIOS16CDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
